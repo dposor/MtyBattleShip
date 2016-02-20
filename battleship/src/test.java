6c311f4cc5185e5b7074d8e6a7877113c1df76ec
finn <dustinposor@gmail.com>
2016-02-20 17:44:33 -0600

    Add fourth change.

--- battleship/src/test.java
+++ battleship/src/test.java
@@ -6,6 +6,7 @@ public class test {
 		System.out.println("This is a test.");
 		System.out.println("This is a second test.");
 		System.out.println("This is a third test.");
+		System.out.println("This is a fourth test.");
 		
 		
 	}
